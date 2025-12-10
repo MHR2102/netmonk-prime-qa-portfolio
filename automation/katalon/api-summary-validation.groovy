85d2b65feb4954a1888ee60b4b502aebd2830f10
MHR2102 <102456122+MHR2102@users.noreply.github.com>
2025-12-10 15:05:05 +0700

    Update api-summary-validation.groovy

--- automation/katalon/api-summary-validation.groovy
+++ automation/katalon/api-summary-validation.groovy
@@ -1,1 +1,8 @@
+ResponseObject resp = WS.sendRequest(findTestObject('API/GetSummary'))
+WS.verifyResponseStatusCode(resp, 200)
 
+def json = new groovy.json.JsonSlurper().parseText(resp.responseBodyContent)
+
+assert json.latency_avg >= 0
+assert json.uptime >= 0 && json.uptime <= 100
+assert ['GREEN','YELLOW','RED'].contains(json.sla_status)
